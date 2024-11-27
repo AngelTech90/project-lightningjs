45ab2eb62068dfe286f91f643c93d63e407e0313
Ange_Tech(Angel_Molina) <angelmolinaimas@gmail.com>
2024-11-27 01:08:40 -0400

    Fixxing troubles of setting enviroment

--- project-lightningjs.git/src/server/NodeJs/server.js
+++ project-lightningjs.git/src/server/NodeJs/server.js
@@ -1,17 +1,19 @@
-import express
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const express = require('express');
-const path = require('path');
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const app = express();
-const port = 8080;
+const app = express()
+const port = 8080
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.listen(port, () => {
-    console.log(`server hosted at http://localhost:${port}`);
-});
+  console.log(`server hosted at http://localhost:${port}`)
+})
